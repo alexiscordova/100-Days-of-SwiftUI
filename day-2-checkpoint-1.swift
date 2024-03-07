4df058d41f0512fa99c4b9f533c4715a4457250f
Alexis Córdova <acordova@mac.com>
2024-03-06 19:27:03 -0800

    Create day-2-checkpoint-1.swift

---
+++ day-2-checkpoint-1.swift
@@ -0,0 +1,9 @@
+import Cocoa
+
+let tempCelsius: Double = 40
+let tempFahrenheit = ((tempCelsius * 9) / 5) + 32
+
+print("""
+Temperature in Celsius: \(tempCelsius)
+Temperature in Fahrenheit: \(tempFahrenheit)
+""")
