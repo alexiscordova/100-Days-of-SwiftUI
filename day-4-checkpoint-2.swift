e2855c0dcbb325a36b1e08fd1706913428f5bad4
Alexis Córdova <acordova@mac.com>
2024-03-06 19:31:49 -0800

    Create day-4-checkpoint-2.swift

---
+++ day-4-checkpoint-2.swift
@@ -0,0 +1,6 @@
+import Cocoa
+
+let albums = ["Debut", "Fearless", "Fearless", "Speak Now", "Speak Now", "Red", "Red", "1989", "1989", "Reputation", "Lover", "Folklore", "Evermore", "Midnights", "The Tortured Poets Society"]
+
+print("Total Taylor Swift Albums: \(albums.count)")
+print("Total Unique Taylor Swift Albums: \(Set(albums).count)")
