642aaad62eb5c72ce63e3d7363352e32f3be3dd4
Alexis Córdova <acordova@mac.com>
2024-03-20 18:30:58 -0700

    Refactor day-11-checkpoint-6.swift

--- day-11-checkpoint-6.swift
+++ day-11-checkpoint-6.swift
@@ -1,25 +1,36 @@
 import Cocoa
 
+enum changeGearDirections {
+    case up, down
+}
+
 struct Car {
     let model: String
     let seats: Int
+    let maxGear = 5
     private(set) var currentGear: Int = 0 {
         didSet {
-            print("New current gear: \(currentGear)")
+            print("""
+Gear changed!
+New current gear: \(currentGear)
+""")
         }
     }
     
-    mutating func changeGears(_ gearChange: Int) {
-        if currentGear + gearChange > 0 && currentGear + gearChange <= 10 {
-            print("Gear successfully changed to \(gearChange)")
-            currentGear += gearChange
+    mutating func changeGears(_ gearChange: changeGearDirections) {
+        if gearChange == changeGearDirections.up && currentGear < maxGear {
+            currentGear += 1
+        } else if gearChange == changeGearDirections.down && currentGear > 0 {
+            currentGear -= 1
         } else {
-            print("Cannot change gear to \(currentGear + gearChange)")
+            print("Cannot change gears")
         }
     }
 }
 
 var mySubaru = Car(model: "Crosstrek", seats: 5)
-mySubaru.changeGears(2)
-mySubaru.changeGears(5)
-mySubaru.changeGears(-7) // this wont work because currentGear == 0
+mySubaru.changeGears(changeGearDirections.up)
+mySubaru.changeGears(changeGearDirections.up)
+mySubaru.changeGears(changeGearDirections.down)
+mySubaru.changeGears(changeGearDirections.down)
+mySubaru.changeGears(changeGearDirections.down) // Won't work, currentGear == 0
