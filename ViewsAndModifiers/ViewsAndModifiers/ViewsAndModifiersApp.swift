6c7b819a4a23836ff9840c22044af81eb6622b84
Alexis Córdova <acordova@mac.com>
2024-06-12 09:39:28 -0700

    Update Xcode UI files

---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Alexis Córdova on 4/26/24.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
